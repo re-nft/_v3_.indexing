4b14bf92379c503f5722ca9faac6f6d3fe7ef8f0
nazariyv <hi@naz.ooo>
2023-09-04 18:29:32 +0100

    chore: refactors

--- src/polygon-mumbai/src/db.ts
+++
@@ -1,4 +0,0 @@
-import {Store as Store_, TypeormDatabase} from '@subsquid/typeorm-store'
-
-export let db = new TypeormDatabase()
-export type Store = Store_
--- src/polygon-mumbai/src/model/generated/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./block.model"
-export * from "./transaction.model"
-export * from "./rentalFactoryEventRentalSafeDeployment.model"
-export * from "./rentalManagerEventRentalStarted.model"
-export * from "./rentalManagerEventRentalStopped.model"
-export * from "./rentalManagerFunctionRentFromZone.model"
-export * from "./rentalManagerFunctionSetZone.model"
