694fffa5f3e8678cbe311058c70bbc96337b1bc4
nazariyv <hi@naz.ooo>
2023-09-13 12:14:42 +0100

    style: formatting migrations

--- db/migrations/1694018219696-Data.js
+++ db/migrations/1694018219696-Data.js
@@ -2,45 +2,71 @@ module.exports = class Data1694018219696 {
     name = 'Data1694018219696'
 
     async up(db) {
-        await db.query(`CREATE TABLE "block" ("id" character varying NOT NULL, "network" text NOT NULL, "number" integer NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, CONSTRAINT "PK_d0925763efb591c2e2ffb267572" PRIMARY KEY ("id"))`)
-        await db.query(`CREATE INDEX "IDX_ea1ca207644ac464e727327c38" ON "block" ("network") `)
-        await db.query(`CREATE INDEX "IDX_38414873c187a3e0c7943bc4c7" ON "block" ("number") `)
-        await db.query(`CREATE INDEX "IDX_5c67cbcf4960c1a39e5fe25e87" ON "block" ("timestamp") `)
-        await db.query(`CREATE TABLE "transaction" ("id" character varying NOT NULL, "network" text NOT NULL, "block_number" integer, "block_timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "hash" text NOT NULL, "to" text, "from" text, "status" integer, CONSTRAINT "PK_89eadb93a89810556e1cbcd6ab9" PRIMARY KEY ("id"))`)
-        await db.query(`CREATE INDEX "IDX_05e00879718cfc101c744472ad" ON "transaction" ("network") `)
-        await db.query(`CREATE INDEX "IDX_2d99bb5a0ab5fb8cf8b746eb39" ON "transaction" ("block_number") `)
-        await db.query(`CREATE INDEX "IDX_bf7f889412fc52430b609e70b4" ON "transaction" ("block_timestamp") `)
-        await db.query(`CREATE INDEX "IDX_de4f0899c41c688529784bc443" ON "transaction" ("hash") `)
-        await db.query(`CREATE INDEX "IDX_1713783ebe978fa2ae9654e4bb" ON "transaction" ("to") `)
-        await db.query(`CREATE INDEX "IDX_290df3897fac99713afb5f3d7a" ON "transaction" ("from") `)
-        await db.query(`CREATE INDEX "IDX_63f749fc7f7178ae1ad85d3b95" ON "transaction" ("status") `)
-        await db.query(`CREATE TABLE "rental_factory_event_rental_safe_deployment" ("id" character varying NOT NULL, "network" text NOT NULL, "block_number" integer NOT NULL, "block_timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "transaction_hash" text NOT NULL, "contract" text NOT NULL, "event_name" text NOT NULL, "safe" text NOT NULL, "rental_manager" text NOT NULL, "guard" text NOT NULL, "owner" text NOT NULL, "nonce" numeric NOT NULL, CONSTRAINT "PK_11953556bcc549e2ab3ab797b95" PRIMARY KEY ("id"))`)
-        await db.query(`CREATE INDEX "IDX_0a9318b529df87dcffafa03275" ON "rental_factory_event_rental_safe_deployment" ("network") `)
-        await db.query(`CREATE INDEX "IDX_976ca4202b9ee7d0eb4a6584ed" ON "rental_factory_event_rental_safe_deployment" ("block_number") `)
-        await db.query(`CREATE INDEX "IDX_4dbf2f7c0e359a9e85dd3cb027" ON "rental_factory_event_rental_safe_deployment" ("block_timestamp") `)
-        await db.query(`CREATE INDEX "IDX_bac208890a97ca35166efde91e" ON "rental_factory_event_rental_safe_deployment" ("transaction_hash") `)
-        await db.query(`CREATE INDEX "IDX_f183c2957f91a03a962325d2dc" ON "rental_factory_event_rental_safe_deployment" ("contract") `)
-        await db.query(`CREATE INDEX "IDX_210599031edd0c23af9599c867" ON "rental_factory_event_rental_safe_deployment" ("event_name") `)
-        await db.query(`CREATE TABLE "rental_manager_event_rental_started" ("id" character varying NOT NULL, "network" text NOT NULL, "block_number" integer NOT NULL, "block_timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "transaction_hash" text NOT NULL, "contract" text NOT NULL, "event_name" text NOT NULL, "seaport_order_hash" text NOT NULL, "renter_wallet" text NOT NULL, "lender" text NOT NULL, "collection" text NOT NULL, "token_id" numeric NOT NULL, "fulfiller" text NOT NULL, "rent_end_timestamp" numeric NOT NULL, CONSTRAINT "PK_93c08cec0e4ef56f487e60ca192" PRIMARY KEY ("id"))`)
-        await db.query(`CREATE INDEX "IDX_e7d23818ef694563444e1d9537" ON "rental_manager_event_rental_started" ("network") `)
-        await db.query(`CREATE INDEX "IDX_49e64a6e6969e2f32402029987" ON "rental_manager_event_rental_started" ("block_number") `)
-        await db.query(`CREATE INDEX "IDX_de3b2afb6b11b26e2073d9fff2" ON "rental_manager_event_rental_started" ("block_timestamp") `)
-        await db.query(`CREATE INDEX "IDX_09553b1f7dcfbf210582944e3f" ON "rental_manager_event_rental_started" ("transaction_hash") `)
-        await db.query(`CREATE INDEX "IDX_5b21bc0b603d2815115a589cf7" ON "rental_manager_event_rental_started" ("contract") `)
-        await db.query(`CREATE INDEX "IDX_da6522085a7fcf12cbac7215d4" ON "rental_manager_event_rental_started" ("event_name") `)
-        await db.query(`CREATE INDEX "IDX_e7fc3848caa18bdabf5d0c049b" ON "rental_manager_event_rental_started" ("seaport_order_hash") `)
-        await db.query(`CREATE INDEX "IDX_4f5856879e432fd5ab4a9ac632" ON "rental_manager_event_rental_started" ("renter_wallet") `)
-        await db.query(`CREATE INDEX "IDX_36043e4b164876651e5530e33a" ON "rental_manager_event_rental_started" ("lender") `)
-        await db.query(`CREATE TABLE "rental_manager_event_rental_stopped" ("id" character varying NOT NULL, "network" text NOT NULL, "block_number" integer NOT NULL, "block_timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "transaction_hash" text NOT NULL, "contract" text NOT NULL, "event_name" text NOT NULL, "seaport_order_hash" text NOT NULL, "renter_wallet" text NOT NULL, "lender" text NOT NULL, "collection" text NOT NULL, "token_id" numeric NOT NULL, "stopper" text NOT NULL, CONSTRAINT "PK_1c96a0fa06dd8c58980e1a15ebb" PRIMARY KEY ("id"))`)
-        await db.query(`CREATE INDEX "IDX_000f943e282b4be072acb0f20d" ON "rental_manager_event_rental_stopped" ("network") `)
-        await db.query(`CREATE INDEX "IDX_5fd8e32d8373bc88597adf8c80" ON "rental_manager_event_rental_stopped" ("block_number") `)
-        await db.query(`CREATE INDEX "IDX_6032b6468b189df089ae95c211" ON "rental_manager_event_rental_stopped" ("block_timestamp") `)
-        await db.query(`CREATE INDEX "IDX_a345aaacd5ba7f08ae41fdac84" ON "rental_manager_event_rental_stopped" ("transaction_hash") `)
-        await db.query(`CREATE INDEX "IDX_6bea44535a9402e40b0f64fdf4" ON "rental_manager_event_rental_stopped" ("contract") `)
-        await db.query(`CREATE INDEX "IDX_894c8edecd3784f52629be5c82" ON "rental_manager_event_rental_stopped" ("event_name") `)
-        await db.query(`CREATE INDEX "IDX_b3265482f3ebf67335a7c2465c" ON "rental_manager_event_rental_stopped" ("seaport_order_hash") `)
-        await db.query(`CREATE INDEX "IDX_5b2e1f5d88343b024105328d64" ON "rental_manager_event_rental_stopped" ("renter_wallet") `)
-        await db.query(`CREATE INDEX "IDX_bd77a079cc7ed58dc92bc6e448" ON "rental_manager_event_rental_stopped" ("lender") `)
+        await db.query(`CREATE TABLE "block" 
+        ("id" character varying NOT NULL, "network" text NOT NULL, 
+         "number" integer NOT NULL, 
+         "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, 
+         CONSTRAINT "PK_d0925763efb591c2e2ffb267572" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE INDEX "IDX_ea1ca207644ac464e727327c38" ON "block" ("network")`)
+        await db.query(`CREATE INDEX "IDX_5c67cbcf4960c1a39e5fe25e87" ON "block" ("timestamp")`)
+        await db.query(`CREATE TABLE "transaction" 
+                ("id" character varying NOT NULL, "network" text NOT NULL, 
+                "block_number" integer, 
+                "block_timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, 
+                "hash" text NOT NULL, "to" text, "from" text, "status" integer, 
+                CONSTRAINT "PK_89eadb93a89810556e1cbcd6ab9" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE INDEX "IDX_05e00879718cfc101c744472ad" ON "transaction" ("network")`)
+        await db.query(`CREATE INDEX "IDX_2d99bb5a0ab5fb8cf8b746eb39" 
+                ON "transaction" ("block_number")`)
+        await db.query(`CREATE INDEX "IDX_bf7f889412fc52430b609e70b4" 
+                ON "transaction" ("block_timestamp")`)
+        await db.query(`CREATE INDEX "IDX_de4f0899c41c688529784bc443" ON "transaction" ("hash")`)
+        await db.query(`CREATE INDEX "IDX_1713783ebe978fa2ae9654e4bb" ON "transaction" ("to")`)
+        await db.query(`CREATE INDEX "IDX_290df3897fac99713afb5f3d7a" ON "transaction" ("from")`)
+        await db.query(`CREATE INDEX "IDX_63f749fc7f7178ae1ad85d3b95" ON "transaction" ("status")`)
+        await db.query(`CREATE TABLE "rental_factory_event_rental_safe_deployment" 
+                ("id" character varying NOT NULL, "network" text NOT NULL, 
+                "block_number" integer NOT NULL, 
+                "block_timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, 
+                "transaction_hash" text NOT NULL, "contract" text NOT NULL, 
+                "event_name" text NOT NULL, "safe" text NOT NULL, 
+                "rental_manager" text NOT NULL, "guard" text NOT NULL, 
+                "owner" text NOT NULL, "nonce" numeric NOT NULL, 
+                CONSTRAINT "PK_11953556bcc549e2ab3ab797b95" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE INDEX "IDX_0a9318b529df87dcffafa03275" 
+                ON "rental_factory_event_rental_safe_deployment" ("network")`)
+        await db.query(`CREATE INDEX "IDX_976ca4202b9ee7d0eb4a6584ed" 
+                ON "rental_factory_event_rental_safe_deployment" ("block_number")`)
+        await db.query(`CREATE INDEX "IDX_4dbf2f7c0e359a9e85dd3cb027" 
+                ON "rental_factory_event_rental_safe_deployment" ("block_timestamp")`)
+        await db.query(`CREATE INDEX "IDX_bac208890a97ca35166efde91e" 
+                ON "rental_factory_event_rental_safe_deployment" ("transaction_hash")`)
+        await db.query(`CREATE INDEX "IDX_f183c2957f91a03a962325d2dc" 
+                ON "rental_factory_event_rental_safe_deployment" ("contract")`)
+        await db.query(`CREATE INDEX "IDX_210599031edd0c23af9599c867" 
+                ON "rental_factory_event_rental_safe_deployment" ("event_name")`)
+        await db.query(`CREATE TABLE "rental_manager_event_rental_started" 
+                ("id" character varying NOT NULL, "network" text NOT NULL, 
+                "block_number" integer NOT NULL, 
+                "block_timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, 
+                "transaction_hash" text NOT NULL, "contract" text NOT NULL, 
+                "event_name" text NOT NULL, "seaport_order_hash" text NOT NULL, 
+                "renter_wallet" text NOT NULL, "lender" text NOT NULL, 
+                "collection" text NOT NULL, "token_id" numeric NOT NULL, 
+                "fulfiller" text NOT NULL, "rent_end_timestamp" numeric NOT NULL, 
+                CONSTRAINT "PK_93c08cec0e4ef56f487e60ca192" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE INDEX "IDX_e7d23818ef694563444e1d9537" 
+                ON "rental_manager_event_rental_started" ("network")`)
+        await db.query(`CREATE INDEX "IDX_49e64a6e6969e2f3240cb5d84f" 
+                ON "rental_manager_event_rental_started" ("block_number")`)
+        await db.query(`CREATE INDEX "IDX_4ad7237c6a19aeb0a9b7a9c5ee" 
+                ON "rental_manager_event_rental_started" ("block_timestamp")`)
+        await db.query(`CREATE INDEX "IDX_c5ef4e5a1c780de60369557f0c" 
+                ON "rental_manager_event_rental_started" ("transaction_hash")`)
+        await db.query(`CREATE INDEX "IDX_314b5127f9d3474c87336e0cf4" 
+                ON "rental_manager_event_rental_started" ("contract")`)
+        await db.query(`CREATE INDEX "IDX_9b6f36fb7d8f4cf1de1ad8d7e7" 
+                ON "rental_manager_event_rental_started" ("event_name")`)
     }
 
     async down(db) {
