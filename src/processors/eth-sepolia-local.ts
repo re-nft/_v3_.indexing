96756006d6f7732e87cadc4a85a94e3d53054788
Rommert Zijlstra <rommert.thomas@gmail.com>
2023-10-12 13:24:28 +0200

    chore: update `dbOptions.stateSchema` declaration

--- src/processors/eth-sepolia-local.ts
+++ src/processors/eth-sepolia-local.ts
@@ -4,7 +4,6 @@ import { NETWORK } from "../consts";
 import { start } from "../processor";
 
 start({
-  dbOptions: { stateSchema: "eth_sepolia_processor" },
   // I went through forked blocks here:
   // https://sepolia.etherscan.io/blocks_forked?p=1 and I could only find one
   // case where re-org depth exceeded 5 blocks: 22 blocks if re-org happens, I
@@ -12,6 +11,7 @@ start({
   // but if error happens, all we would have to do is re-run / re-deploy the
   // squid. So re-deploying once a year (for sepolia) is fine.
   finalityConfirmation: 6,
+  dbOptions: { stateSchema: "eth_sepolia_fork_processor" },
   network: NETWORK.ETH_SEPOLIA,
   rentalFactoryAddress: "0x2c2BBA22aA19Ba34bC5BA65e6c35Ce54DA36A33D",
   rentalManagerAddress: "0xea0b609F81B3D7699a970e670ec471Daf687E5c2",
