ab3061b2c7a48e4c4440db70392179f3da71d643
Rommert Zijlstra <rommert.thomas@gmail.com>
2023-10-12 13:24:55 +0200

    chore: set Sepolia fork `finalityConfirmation` to `1`

--- src/processors/eth-sepolia-local.ts
+++ src/processors/eth-sepolia-local.ts
@@ -1,17 +1,15 @@
+// Keeping this around for reference. See commented out `archive` option below.
+// eslint-disable-next-line
 import { lookupArchive } from "@subsquid/archive-registry";
 
 import { NETWORK } from "../consts";
 import { start } from "../processor";
 
+// TODO: make this a two-pass process where first we index from startBlock to
+// endBlock on the `archive` and afterwards start a processer on the `chain`.
 start({
-  // I went through forked blocks here:
-  // https://sepolia.etherscan.io/blocks_forked?p=1 and I could only find one
-  // case where re-org depth exceeded 5 blocks: 22 blocks if re-org happens, I
-  // am not sure about this, because squid is supposed to handle automatically,
-  // but if error happens, all we would have to do is re-run / re-deploy the
-  // squid. So re-deploying once a year (for sepolia) is fine.
-  finalityConfirmation: 6,
   dbOptions: { stateSchema: "eth_sepolia_fork_processor" },
+  finalityConfirmation: 1,
   network: NETWORK.ETH_SEPOLIA,
   rentalFactoryAddress: "0x2c2BBA22aA19Ba34bC5BA65e6c35Ce54DA36A33D",
   rentalManagerAddress: "0xea0b609F81B3D7699a970e670ec471Daf687E5c2",
