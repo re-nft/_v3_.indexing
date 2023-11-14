c24e0a4bc7a92a3bc67166f111be42355665426c
Nazariy Vavryk <hi@naz.ooo>
2023-11-14 20:46:08 +0000

    feat: non-flaky local fork indexing plus comment

--- src/processors/eth-sepolia-fork/config.ts
+++ src/processors/eth-sepolia-fork/config.ts
@@ -3,7 +3,9 @@ import { type EvmIndexerOptions } from "../../processor";
 
 export const config: EvmIndexerOptions = {
   dbOptions: { stateSchema: "eth_sepolia_fork_processor" },
-  finalityConfirmation: 1,
+  // DO NOT CHANGE THIS TO ANY OTHER NUMBER
+  // YOUR FORK INDEXING WILL BECOME FLAKY AND WILL NOT WORK
+  finalityConfirmation: 0,
   network: NETWORK.ETH_SEPOLIA_FORK,
   createAddress: "0xc60f6eb59002a4F71A3e34d2C5Bf9c244D42BD68",
   stopAddress: "0xa95BC23c95e62Dd0a9dc85790e45423C98069546",
